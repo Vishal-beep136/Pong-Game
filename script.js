7f604ae07cb778502c5ea9d5de76520b57e89eb9
Vishal Beep <alaphtech@gmail.com>
2022-03-20 09:50:30 +0530

    ping pong game js

---
+++ script.js
@@ -0,0 +1,59 @@
+import Ball from "./Ball.js"
+import Paddle from "./Paddle.js"
+
+const ball = new Ball(document.getElementById("ball"));
+const playerPaddle = new Paddle(document.getElementById("player-paddle"));
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"));
+const playerScoreElem = document.getElementById("player-score");
+const computerScoreElem = document.getElementById("computer-score");
+
+
+const wonSound = new Audio("./lose-sound.wav");
+
+
+let lastTime
+function update(time) {
+    if (lastTime != null) {
+        const delta = time - lastTime
+        ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()])
+        computerPaddle.update(delta, ball.y)
+        const hue = parseFloat(
+            getComputedStyle(document.documentElement).getPropertyValue("--hue")
+        )
+
+        document.documentElement.style.setProperty("--hue", hue + delta * 0.01)
+
+        if (isLose()) handleLose()
+    }
+
+
+    lastTime = time
+    window.requestAnimationFrame(update)
+}
+
+function isLose() {
+    const rect = ball.rect()
+    return rect.right >= window.innerWidth || rect.left <= 0
+}
+
+function handleLose() {
+    const rect = ball.rect()
+    if (rect.right >= window.innerWidth) {
+        //here player won
+        wonSound.play();
+        playerScoreElem.textContent = parseInt(playerScoreElem.textContent) + 1;
+    } else {
+        //here computer won
+        wonSound.play();
+        computerScoreElem.textContent = parseInt(computerScoreElem.textContent) + 1;
+    }
+    ball.reset()
+    computerPaddle.reset()
+}
+
+
+document.addEventListener("mousemove", e => {
+    playerPaddle.position = (e.y / window.innerHeight) * 100
+})
+
+window.requestAnimationFrame(update)
